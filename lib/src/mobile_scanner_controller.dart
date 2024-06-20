b16c0fc1d32b302e246d569d5023c0b8b80bc763
Dwiky Hardiansah <dwikyhardiansah93@gmail.com>
2024-06-20 23:48:10 +0700

    bump version and adding some method

--- lib/src/mobile_scanner_controller.dart
+++ lib/src/mobile_scanner_controller.dart
@@ -420,4 +420,10 @@ class MobileScannerController extends ValueNotifier<MobileScannerState> {
 
     await MobileScannerPlatform.instance.dispose();
   }
+
+  void addBarcode(BarcodeCapture? barcode) {
+    if (!_barcodesController.isClosed && barcode != null) {
+      _barcodesController.add(barcode);
+    }
+  }
 }
